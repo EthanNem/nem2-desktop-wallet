b791619d2a82d542b0058a737f6837c677f122b1
fandi <214333890@qq.com>
2020-02-21 18:06:35 +0800

    Correct folder name

--- src/views/pages/accounts/CreationPage/GenerateMnemonic/GenerateMnemonicTs.ts
+++ src/views/pages/accounts/CreationPage/GenerateMnemonic/GenerateMnemonicTs.ts
@@ -1,22 +1,6 @@
 import {Vue, Component} from 'vue-property-decorator'
-// @ts-ignore
-import ProgressDisplay from '@/components/ProgressDisplay/ProgressDisplay.vue'
 
-@Component({
-  components: {
-    ProgressDisplay,
-  },
-})
+@Component
 export default class GenerateMnemonicTs extends Vue {
-  value: number = 0
-  addValue() {
-    if (this.value === 100) {
-      return setTimeout(this.submit, 500)
-    }
-    this.value ++
-  }
 
-  submit() {
-    this.$router.push({name: 'accounts.creation.backupMnemonic'})
-  }
 }
--- src/views/pages/accounts/ImportPage/ImportPageTs.ts
+++ src/views/pages/accounts/ImportPage/ImportPageTs.ts
@@ -1,28 +1,6 @@
 import {Vue, Component} from 'vue-property-decorator'
-// @ts-ignore
-import StepBar from '@/components/StepBar/StepBar.vue'
 
-@Component({
-  components: {
-    StepBar,
-  },
-})
+@Component
 export default class ImportPageTs extends Vue {
-  stepList = [
-    {
-      index: 1,
-      text: '导入助记词',
-      isChild: false,
-    },
-    {
-      index: 2,
-      text: '设置账户',
-      isChild: false,
-    },
-    {
-      index: 3,
-      text: '导入钱包',
-      isChild: false,
-    },
-  ]
+
 }
