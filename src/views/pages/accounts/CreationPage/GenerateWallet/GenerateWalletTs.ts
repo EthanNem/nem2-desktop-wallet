b6a9c75f91b2176efd319b1647537f5d6539f391
ghc <330125849@qq.com>
2020-02-22 14:06:26 +0800

    change menmonic component

--- src/views/pages/accounts/CreationPage/GenerateWallet/GenerateWalletTs.ts
+++ src/views/pages/accounts/CreationPage/GenerateWallet/GenerateWalletTs.ts
@@ -1,15 +1,12 @@
 import {Vue, Component} from 'vue-property-decorator'
-// @ts-ignore
-import RightTipDisplay from '@/components/RightTipDisplay/RightTipDisplay.vue'
+//@ts-ignore
+import SelectAccountTable from '@/components/SelectAccountTable/SelectAccountTable.vue'
+//@ts-ignore
+import TableTitle from '@/components/TableTitle/TableTitle.vue'
 
 @Component({
-  components: {
-    RightTipDisplay,
-  },
+    components: {TableTitle,SelectAccountTable}
 })
-export default class BackupMnemonicTs extends Vue {
-  tipContents: string[] = [
-    '请不要透露你备份的记助词，任何人有了记助词，将可以彻底掌控其下的钱包资产',
-    '请将你的记助词离线写在纸上并做多份保存，或者离线的加密磁盘和存储介质等',
-  ]
+export default class GenerateWalletTs extends Vue {
+
 }
