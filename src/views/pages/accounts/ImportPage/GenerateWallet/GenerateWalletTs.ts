57bfe4c6bbaf5e57cfbe85a0a81c97eef2c40736
ghc <330125849@qq.com>
2020-02-22 14:06:26 +0800

    change menmonic component

--- src/views/pages/accounts/ImportPage/GenerateWallet/GenerateWalletTs.ts
+++ src/views/pages/accounts/ImportPage/GenerateWallet/GenerateWalletTs.ts
@@ -1,4 +1,7 @@
 import {Vue, Component} from 'vue-property-decorator'
+//child component
+//@ts-ignore
+import ImportMnemonic from '@/components/ImportMnemonic/ImportMnemonic.vue'
 
 @Component
 export default class GenerateWalletTs extends Vue {
