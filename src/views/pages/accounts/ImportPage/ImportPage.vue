94d2756ad70ffeea5250b03cbbcc1d8869d408f8
ghc <330125849@qq.com>
2020-02-24 12:10:50 +0800

    fix

--- src/views/pages/accounts/ImportPage/ImportPage.vue
+++ src/views/pages/accounts/ImportPage/ImportPage.vue
@@ -5,19 +5,6 @@
     </div>
     <div class="page-detail-container">
       <div v-if="$route.meta.title.length !== 0" class="page-content-title">{{ $route.meta.title }}</div>
-      <div class="import-wallet">
-        <!-- <p>导入钱包</p> -->
-        <div class="select-network">
-          <div class="title">区块链网络:</div>
-          <input type="text" />
-        </div>
-        <div class="select-network">
-          <div class="title">网络类型:</div>
-          <Select name class>
-            <Option>TEXT-NET</Option>
-          </Select>
-        </div>
-      </div>
       <!--<div class="page-content-title">
         验证备份的助记词
       </div>-->
